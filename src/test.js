758026227dac7d3636440139d3ba09d9f2266ad5
Rahul Rajput <rahul@Rahuls-MacBook-Pro.local>
2022-05-07 11:30:14 +0530

    fix: bug fix

--- src/test.js
+++ src/test.js
@@ -102,15 +102,17 @@ app.post("/start-assignment", async (req, res) => {
 })
 
 app.post("/register-assignment",async(req,res)=>{
-    let assignment_data = req.body
+    let user = req.body.user;
+    let assignment_data = req.body.assignment_data
+
     await Assignment.create(assignment_data) 
-    // try {
-    //     let url = await start_exam(user, assignment)
-    //     res.send(url)
-    // }catch(err) {
-    //     console.log(err.message)
-    //     res.send(err.message)
-    // }
+    try {
+        let url = await start_exam(user, assignment_data)
+        res.send(url)
+    }catch(err) {
+        console.log(err.message)
+        res.send(err.message)
+    }
 })
 app.get("/show-exam",async (req,res) => {
     let data = await Assignment.find()
