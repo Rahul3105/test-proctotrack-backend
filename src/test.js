3590a2e78416281376bd22b3fb15baad9dc3dbce
Rahul Rajput <rahul@Rahuls-MacBook-Pro.local>
2022-05-19 17:01:34 +0530

    feat: redirect dashboard

--- src/test.js
+++ src/test.js
@@ -122,7 +122,39 @@ app.get("/show-exam",async (req,res) => {
     let data = await Assignment.find()
     res.json(data)
 })
-console.log(new Date())
+app.get("/show-dashboard", async(req,res) => {
+    let user = {
+        "first_name" : "rahul",
+        "last_name" : "rajput",
+        "user_id" : "rahul952022",
+        "email": "rahul@masaischool.com",
+        "role" : "Instructor"
+    };
+    let assignment_data = {
+        "id": 3857,
+        "is_proctored": true,
+        "is_onboarding": false,
+        "is_active": true,
+        "name": "Cohort-31 Module-3 Sprint Evaluation-3",
+        "test_url": "https://oj.masaischool.com/contest/3857",
+        "duration": "60",
+        "attempts_allowed": "1",
+        "proctoring_level": "3",
+        "group_id": "3857",
+        "access_code": "3857",
+        "start": "2022-05-09T14:30:00.542Z",
+        "end": "2022-05-09T15:45:00.542Z"
+    } 
+    try {
+        let url = await start_exam(user, assignment_data)
+        res.redirect(url.url)
+    }catch(err) {
+        console.log(err.message)
+        res.send(err.message)
+    }
+})
+
+
 app.listen (port, async () => {
     await connect_with_db()
     console.log("listening on port 4000")
